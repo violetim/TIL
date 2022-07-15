ed3eb5933cbf1afe79aee62c304298ac28c32af6
purpleim <brlimdev@gmail.com>
2022-07-15 14:40:06 +0900

    SC Day7 python intro

---
+++ startcamp/hello.py
@@ -0,0 +1,15 @@
+print('Chippo')
+
+greeting = '반갑습니다.'
+player = 5
+
+
+
+def bot():
+    player = 5
+    #20명 모두에게 인사를 하려면?
+    while player > 0:
+    print(greeting)
+    player = player - 1
+
+
---
+++ startcamp/test.py
@@ -0,0 +1,7 @@
+from hello import greeting, player
+from hello import bot
+
+print(greeting)
+print(player)
+
+bot()
